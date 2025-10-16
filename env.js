eef5d317651bc50f36d051d8e6bbafacc248ea12
skt2256680-dotcom <skt2256680@gmail.com>
2025-10-17 01:11:10 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,6 +2,6 @@
 window.ENV = {
   SUPABASE_URL: "https://pceskbdipmmrfwqeciyv.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjZXNrYmRpcG1tcmZ3cWVjaXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MDg2NDYsImV4cCI6MjA3NjA4NDY0Nn0.UXn62klzDB4c7vZK8GYXWc2gUsnYUoGUUgR7YzGZH78",
-  IMAGE_BUCKET: "picture",   // or "diary-images"
-  VIDEO_BUCKET: "video"      // or "diary-videos"
+  IMAGE_BUCKET: "diary-images",   // or "diary-images"
+  VIDEO_BUCKET: "diary-videos"      // or "diary-videos"
 };
